1ba968f3fa270854b46c3765c5218ec1d694f342
Anushree <anushreekchoudhary@gmail.com>
2026-08-27 22:17:46 +0530

    Add data augmentation transformations for training

--- dataset.py
+++ dataset.py
@@ -20,6 +20,25 @@ print("Total caption rows:", len(df))
 print("Unique images:", df['image'].nunique())
 print("Captions per image:", df.groupby('image').size().value_counts())  # should mostly show 5
 
+sample_tfms = [
+    A.HorizontalFlip(),
+    A.RandomBrightnessContrast(),
+    A.ColorJitter(),
+    A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.3, rotate_limit=45, p=0.5),
+    A.HueSaturationValue(p=0.3),
+]
+train_tfms = A.Compose([
+    *sample_tfms,
+    A.Resize(224,224),
+    A.Normalize(mean=[0.5,0.5,0.5],std=[0.5,0.5,0.5],always_apply=True),
+    ToTensorV2()
+])
+valid_tfms = A.Compose([
+    A.Resize(224,224),
+    A.Normalize(mean=[0.5,0.5,0.5],std=[0.5,0.5,0.5],always_apply=True),
+    ToTensorV2()
+])
+
 class Dataset:
     def __init__(self, df, tfms, dir):
         self.df = df
